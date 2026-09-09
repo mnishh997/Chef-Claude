217fb19601520f1dedaf833845470ca9d7c37763
Manish Kumar <mnishh997@gmail.com>
2026-09-09 21:29:19 +0530

    useState for taking ingredient name and adding to list and rendering on screen

---
+++ public/chef-claude_files/main.jsx
@@ -0,0 +1,13 @@
+const createRoot = __vite__cjsImport0_reactDom_client["createRoot"];const _jsxDEV = __vite__cjsImport3_react_jsxDevRuntime["jsxDEV"];import __vite__cjsImport0_reactDom_client from "/node_modules/.vite/deps/react-dom_client.js?v=98c95088";
+import App from "/src/App.jsx?t=1788966689230";
+import "/src/index.css?t=1788885111264";
+var _jsxFileName = "/Users/mnishh999/Documents/coding/React/Chef Claude/src/main.jsx";
+import __vite__cjsImport3_react_jsxDevRuntime from "/node_modules/.vite/deps/react_jsx-dev-runtime.js?v=98c95088";
+const root = createRoot(document.getElementById("root"));
+root.render(/* @__PURE__ */ _jsxDEV(App, {}, void 0, false, {
+	fileName: _jsxFileName,
+	lineNumber: 5,
+	columnNumber: 13
+}, this));
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJtYXBwaW5ncyI6IkFBQUEsU0FBU0Esa0JBQWtCO0FBQzNCLE9BQU9DLFNBQVM7QUFDaEIsT0FBTzs7O0FBRVAsTUFBTUMsT0FBT0YsV0FBV0csU0FBU0MsZUFBZSxNQUFNLENBQUM7QUFFdkRGLEtBQUtHLE9BQU8sd0JBQUMsS0FBRCxDQUFJOzs7O1FBQUciLCJuYW1lcyI6WyJjcmVhdGVSb290IiwiQXBwIiwicm9vdCIsImRvY3VtZW50IiwiZ2V0RWxlbWVudEJ5SWQiLCJyZW5kZXIiXSwiaWdub3JlTGlzdCI6W10sInNvdXJjZXMiOlsibWFpbi5qc3giXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHsgY3JlYXRlUm9vdCB9IGZyb20gXCJyZWFjdC1kb20vY2xpZW50XCI7XG5pbXBvcnQgQXBwIGZyb20gXCIuL0FwcC5qc3hcIjtcbmltcG9ydCBcIi4vaW5kZXguY3NzXCI7XG5cbmNvbnN0IHJvb3QgPSBjcmVhdGVSb290KGRvY3VtZW50LmdldEVsZW1lbnRCeUlkKFwicm9vdFwiKSk7XG5cbnJvb3QucmVuZGVyKDxBcHAgLz4pO1xuIl0sImZpbGUiOiIvVXNlcnMvbW5pc2hoOTk5L0RvY3VtZW50cy9jb2RpbmcvUmVhY3QvQ2hlZiBDbGF1ZGUvc3JjL21haW4uanN4In0=
--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -1,16 +1,39 @@
+import { useState } from "react";
 import styles from "./MainContent.module.css";
 
-export default function () {
+export default function MainContent() {
+  const [ingredients, setIngredients] = useState([
+    "Mango",
+    "Curd",
+    "Chia Seeds",
+  ]);
+  console.log(ingredients);
+  const ingredientElements = ingredients.map((ingredient) => (
+    <li key={ingredient}>{ingredient}</li>
+  ));
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const newIngredient = formData.get("ingredient");
+    if (newIngredient.length > 0) {
+      setIngredients([...ingredients, newIngredient]);
+      const inputField = document.getElementById("myInput");
+      inputField.value = "";
+    }
+  }
   return (
     <main>
-      <form className={styles.addIngredientForm}>
+      <form onSubmit={handleSubmit} className={styles.addIngredientForm}>
         <input
+          id="myInput"
           type="text"
           placeholder="eg. oregano"
           aria-label="Add ingredient"
+          name="ingredient"
         />
         <button>Add ingredient</button>
       </form>
+      <ul>{ingredientElements}</ul>
     </main>
   );
 }
