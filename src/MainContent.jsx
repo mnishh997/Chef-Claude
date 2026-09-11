aaeb3519c2e4d2b89242218c408bac3c5fa19bd5
Manish Kumar <mnishh997@gmail.com>
2026-09-11 19:51:12 +0530

    Removing ingredient via Cross button implemented (NO CSS)

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -2,20 +2,36 @@ import { useState } from "react";
 import styles from "./MainContent.module.css";
 
 export default function MainContent() {
-  const [ingredients, setIngredients] = useState([
-    "Mango",
-    "Curd",
-    "Chia Seeds",
-  ]);
-  console.log(ingredients);
-  const ingredientElements = ingredients.map((ingredient) => (
-    <li key={ingredient}>{ingredient}</li>
-  ));
+  const [ingredients, setIngredients] = useState([]);
+  // console.log(ingredients);
+  // const ingredientElements = ingredients.map((ingredient) => (
+  //   <li key=>
+  //     {ingredient}
+  //     <button onClick={handleCross}>X</button>
+  //   </li>
+  // ));
+  const reactIngredientList = [];
+  for (let i = 0; i < ingredients.length; i++) {
+    reactIngredientList.push(
+      <li key={i}>
+        {ingredients[i]} <button onClick={() => handleCross(i)}>X</button>
+      </li>,
+    );
+  }
+  function handleCross(index) {
+    // console.log(`Button was clicked with index ${index}`);
+    var updatedIngredients = [];
+    for (let i = 0; i < ingredients.length; i++) {
+      if (i == index) continue;
+      updatedIngredients.push(ingredients[i]);
+    }
+    setIngredients(updatedIngredients);
+  }
   function handleSubmit(event) {
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
     const newIngredient = formData.get("ingredient");
-    if (newIngredient.length > 0) {
+    if (newIngredient.length) {
       setIngredients([...ingredients, newIngredient]);
       const inputField = document.getElementById("myInput");
       inputField.value = "";
@@ -33,7 +49,7 @@ export default function MainContent() {
         />
         <button>Add ingredient</button>
       </form>
-      <ul>{ingredientElements}</ul>
+      <ul>{reactIngredientList}</ul>
     </main>
   );
 }
